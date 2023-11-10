39f4a3016565166b87e9e7bbc48beb1636f29c01
sallfarr <sallfarr@outlook.com>
2023-11-10 22:13:44 +0700

    update

--- main.tf
+++ main.tf
@@ -24,6 +24,15 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+variable "gce_ssh_user" {
+  type    = string
+  default = "root"
+}
+
+variable "gce_ssh_pub_key_file" {
+  type    = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCp0THegmAUdowXe9g+MJIlt8qyBoPiHVUfzKFwEX0W6nrno8HL2tRGYfszodJHfu5PZxyuowjMX8iOeY1rJMOx8UwUceLYZT7knvio/Kapqgxy0cJ92Hy/aw85P5niGpE/w8nmoRuvOfMBQZk8Q8JcRMXrTQIkFI7wlq4yR2MH2OXCkjcqbWQxChThS2xCzFGlkR3qMyWhxMPKiYWFbwXUWxbCS05kabSKeHYY4dg02SwnczRJotfD0od2Lq4rhWKnYgvpWgCKUbvgomTXP/6JqfrGicAAp1l4QT+mDTq7OhoKjxNY6nvuKVmq010MQWTkzXZBhtwamPQNe7XgEU9K0kviOHDhsRRtGfxeZhZWaXOiNKeer4w1MWcsnD0CHUqoZjoNJhDaJBjbHh6xAsOn7O74GHhdxwSth0YJax1+DIH1A9J1Z1yjVE7/DT2X+WvD0iJE6bYGSdKJbFS4Z6sIcG2Bm8PV1jXtfc9hzLOtWPfF55xsVzOajISG5Gf8dkU= sallfarr@DESKTOP-79GAIUS"
+}
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
@@ -36,16 +45,6 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
-variable "var.gce_ssh_user" {
-  type = string
-  default = "root"
-}
-
-variable "gce_ssh_pub_key_file" {
-  type = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCp0THegmAUdowXe9g+MJIlt8qyBoPiHVUfzKFwEX0W6nrno8HL2tRGYfszodJHfu5PZxyuowjMX8iOeY1rJMOx8UwUceLYZT7knvio/Kapqgxy0cJ92Hy/aw85P5niGpE/w8nmoRuvOfMBQZk8Q8JcRMXrTQIkFI7wlq4yR2MH2OXCkjcqbWQxChThS2xCzFGlkR3qMyWhxMPKiYWFbwXUWxbCS05kabSKeHYY4dg02SwnczRJotfD0od2Lq4rhWKnYgvpWgCKUbvgomTXP/6JqfrGicAAp1l4QT+mDTq7OhoKjxNY6nvuKVmq010MQWTkzXZBhtwamPQNe7XgEU9K0kviOHDhsRRtGfxeZhZWaXOiNKeer4w1MWcsnD0CHUqoZjoNJhDaJBjbHh6xAsOn7O74GHhdxwSth0YJax1+DIH1A9J1Z1yjVE7/DT2X+WvD0iJE6bYGSdKJbFS4Z6sIcG2Bm8PV1jXtfc9hzLOtWPfF55xsVzOajISG5Gf8dkU= sallfarr@DESKTOP-79GAIUS"
-}
-
   metadata = {
     ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
   }
@@ -56,11 +55,6 @@ variable "gce_ssh_pub_key_file" {
     }
   }
 }
-
-
-
-
-
 
 
 
